114f830a6064286c1e4410bd2ce6e293c82aa347
voltkigisa <raden.233040043@mail.unpas.ac.id>
2026-05-24 13:09:19 +0700

    enemy and Box

--- BALLER N GLOV/Assets/Baller/Script/EnemyLebah.cs
+++ BALLER N GLOV/Assets/Baller/Script/EnemyLebah.cs
@@ -23,7 +23,7 @@ public class EnemyLebah : MonoBehaviour
         {
             // Terbang ke Kanan
             transform.Translate(Vector2.right * speed * Time.deltaTime);
-            
+
             // Jika sudah terlalu jauh ke kanan, putar balik ke kiri
             if (transform.position.x >= startPosition.x + patrolDistance)
             {
@@ -34,13 +34,16 @@ public class EnemyLebah : MonoBehaviour
         {
             // Terbang ke Kiri
             transform.Translate(Vector2.left * speed * Time.deltaTime);
-            
+
             // Jika sudah terlalu jauh ke kiri, putar balik ke kanan
             if (transform.position.x <= startPosition.x - patrolDistance)
             {
                 movingRight = true;
             }
         }
+        Vector3 scale = transform.localScale;
+        scale.x = movingRight ? -Mathf.Abs(scale.x) : Mathf.Abs(scale.x);
+        transform.localScale = scale;
     }
     // OnTriggerEnter2D digunakan untuk mendeteksi Collider yang statusnya "Is Trigger"
     private void OnTriggerEnter2D(Collider2D collision)
--- BALLER N GLOV/Assets/Mail/MainMenu.cs
+++ BALLER N GLOV/Assets/Mail/MainMenu.cs
@@ -1,16 +1,16 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
-    public string gameSceneName = "GameScene";
-    public void Exit()
-    {
-        Application.Quit();
-    }
+// public class MainMenu : MonoBehaviour
+// {
+//     public string gameSceneName = "GameScene";
+//     public void Exit()
+//     {
+//         Application.Quit();
+//     }
 
-    public void StartGame()
-    {
-        SceneManager.LoadScene(gameSceneName);
-    }
-}
+//     public void StartGame()
+//     {
+//         SceneManager.LoadScene(gameSceneName);
+//     }
+// }
