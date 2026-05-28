79069e8b054e9d8aa59135ac23d2574ee0c43319
voltkigisa <raden.233040043@mail.unpas.ac.id>
2026-05-28 16:57:10 +0700

    switch preasure plate

--- BALLER N GLOV/Assets/Baller/Script/PressurePlate.cs
+++ BALLER N GLOV/Assets/Baller/Script/PressurePlate.cs
@@ -2,14 +2,35 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
+    [Header("Pintu")]
     public DoorController targetDoor;
+
+    [Header("Pengaturan Gerak Plate")]
+    public float pressedOffsetY = -0.2f;
+    public float moveSpeed = 5f;
+
     private int insideCount = 0;
+    private Vector3 startPosition;
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+        startPosition = transform.localPosition;
+        targetPosition = startPosition;
+    }
+
+    void Update()
+    {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * moveSpeed);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        //Debug.Log("Trigger masuk: " + other.gameObject.name + " tag: " + other.tag);
         if (other.CompareTag("Player") || other.CompareTag("Kotak"))
         {
             insideCount++;
+            targetPosition = new Vector3(startPosition.x, startPosition.y + pressedOffsetY, startPosition.z);
             if (targetDoor != null)
                 targetDoor.SetOpen(true);
         }
@@ -20,10 +41,10 @@ public class PressurePlate : MonoBehaviour
         if (other.CompareTag("Player") || other.CompareTag("Kotak"))
         {
             insideCount--;
-
             if (insideCount <= 0)
             {
                 insideCount = 0;
+                targetPosition = startPosition;
                 if (targetDoor != null)
                     targetDoor.SetOpen(false);
             }
